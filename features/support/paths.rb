9256f18b1f677b798964efbbac047662d9886ff3
Nicola Prando <nicola.prando@intesys.it>
2012-06-29 17:40:22 +0200

    Added title funzionality for show page

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,7 +15,8 @@ module NavigationHelpers
   #
     when /^(.*)'s profile page$/i
       user_profile_path(User.find_by_login($1))
-  
+    when /the project page for "([^\"]*)"/
+      project_path(Project.find_by_name!($1))
     else
       begin
         page_name =~ /^the (.*) page$/
